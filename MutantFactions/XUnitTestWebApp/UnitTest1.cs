2be84f0117f54ae5f1ca7c2a55ba512f28093945
Ariel R. Pedraza <pedrazaa2@gmail.com>
2018-01-18 11:28:50 -0800

    Added unit tests

--- MutantFactions/XUnitTestWebApp/UnitTest1.cs
+++ MutantFactions/XUnitTestWebApp/UnitTest1.cs
@@ -1,14 +1,67 @@
 using System;
 using Xunit;
+using MutantFactions.Data;
+using MutantFactions.Controllers;
+using MutantFactions.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace XUnitTestWebApp
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        MutantDbContext _context;
 
+        DbContextOptions<MutantDbContext> options = new DbContextOptionsBuilder<MutantDbContext>()
+                                                        .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                                                        .Options;
+        Student mutant = new Student
+        {
+            RealName = "Scott Summers",
+            CodeName = "Cyclops",
+            MutantPower = "Concussive Optic Blasts",
+            MutantFaction = "X-Men"
+        };
+
+        [Fact]
+        public void TestModelProperties()
+        {
+            Assert.Equal("Scott Summers", mutant.RealName);
+            Assert.Equal("Cyclops", mutant.CodeName);
+            Assert.Equal("Concussive Optic Blasts", mutant.MutantPower);
+            Assert.Equal("X-Men", mutant.MutantFaction);
+        }
+
+        [Fact]
+        public void TestHomeIndexReturnsView()
+        {
+            using (_context = new MutantDbContext(options))
+            {
+                _context.MutantDB.Add(mutant);
+                _context.SaveChanges();
+                // Arrange
+                HomeController controller = new HomeController(_context);
+                // Act
+                var result = controller.Index();
+                // Assert
+                Assert.NotNull(result);
+            }
+        }
+
+        [Fact]
+        public void TestHomeDetailsReturnsView()
+        {
+            using (_context = new MutantDbContext(options))
+            {
+                _context.MutantDB.Add(mutant);
+                _context.SaveChanges();
+                // Arrange
+                HomeController controller = new HomeController(_context);
+                // Act
+                var result = controller.Details();
+                // Assert
+                Assert.NotNull(result);
+            }
         }
     }
+
 }
