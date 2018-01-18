5859868934a18c1e2f05b5902d4430effe3f9ccf
jeffxmartinez <jeffxmartinez@gmail.com>
2018-01-18 11:02:50 -0800

    Testing models

--- EntityFramework/XUnitTestProject1/UnitTest1.cs
+++ EntityFramework/XUnitTestProject1/UnitTest1.cs
@@ -2,17 +2,50 @@ using System;
 using Xunit;
 using EntityFramework.Controllers;
 using System.Threading.Tasks;
+using EntityFramework.Models;
 
 namespace XUnitTestProject1
 {
     public class UnitTest1
     {
-       /* [Fact]
-        public async Task IndexPost()
+       [Fact]
+        public void TestFirstName()
         {
-            Assert.Equal("Jeff", HomeController.Index(hogs));
-        }
-        */
+            Hogwarts s = new Hogwarts();
+            s.FirstName = "Josh";
+            s.FirstName = "Jeff";
 
+            Assert.Equal("Jeff", s.FirstName);
+        }
+
+        [Fact]
+        public void TestLastName()
+        {
+            Hogwarts s = new Hogwarts();
+            s.LastName = "Potter";
+            s.LastName = "Weasley";
+
+            Assert.Equal("Weasley", s.LastName);
+        }
+
+        [Fact]
+        public void TestPhone()
+        {
+            Hogwarts s = new Hogwarts();
+            s.Phone = 253;
+            s.Phone = 352;
+
+            Assert.Equal(352, s.Phone);
+        }
+
+        [Fact]
+        public void TestHouse()
+        {
+            Hogwarts s = new Hogwarts();
+            s.House = "Hufflepuff";
+            s.House = "Gryffindor";
+
+            Assert.Equal("Gryffindor", s.House);
+        }
     }
 }
